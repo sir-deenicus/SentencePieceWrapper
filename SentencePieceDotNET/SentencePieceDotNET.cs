9914cb1769b845290ef627c8dee1ae6fa898860b
Deen Abiola <sir.deenicus@gmail.com>
2020-12-05 18:04:32 +0100

    add C# wrapper

---
+++ SentencePieceDotNET/SentencePieceDotNET.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
+//Follows rough scaffold in https://www.codeproject.com/Articles/18032/How-to-Marshal-a-C-Class 
+
+namespace SentencePieceDotNET
+{
+    public class SentencePieceDotNET : IDisposable
+    {
+        const string dllname = "SentencePieceWrapper.dll";
+
+        [DllImport(dllname)]
+        static private extern IntPtr CreateSentencePieceProcessor();
+
+        [DllImport(dllname)]
+        static private extern void Dispose(IntPtr pointer);
+		
+		[DllImport(dllname)]
+		static private extern void Load(IntPtr pointer, string path);
+
+		[DllImport(dllname)]
+        static private extern void EncodeSentence(IntPtr pSProc, byte[] sentenceBytes, out int idslen, int[] idsout);
+
+        [DllImport(dllname)]
+        static private extern void DecodeSentence(IntPtr pSProc, int len, int[] ids, out int textlen, byte[] textbuffer);
+
+		private IntPtr sentencePieceProcessor;    
+
+		public SentencePieceDotNET()
+		{ 
+			this.sentencePieceProcessor = CreateSentencePieceProcessor();
+		}
+
+		public void Dispose()
+		{
+			Dispose(true);
+		}
+
+		public void Load(string path)
+        {
+			Load(sentencePieceProcessor, path);
+        }
+
+		public int[] Encode(string sentence)
+        {
+            var b = Encoding.Unicode.GetBytes(sentence);
+            var bytes = Encoding.Convert(Encoding.Unicode, Encoding.UTF8, b);
+			var buffer = new int[bytes.Length];
+
+			EncodeSentence(sentencePieceProcessor, bytes, out int len, buffer);
+			return buffer[0..len];
+		}
+		public int[] Encode2(string sentence)
+		{
+			var b = Encoding.Unicode.GetBytes(sentence);
+			var bytes = Encoding.Convert(Encoding.Unicode, Encoding.UTF8, b);
+			var buffer = new int[bytes.Length];
+
+			EncodeSentence(sentencePieceProcessor, bytes, out int len, buffer);
+			return buffer;
+		}
+
+		public string Decode(int[] ids)
+        {
+            var textbuffer = new byte[ids.Length * 50];
+            DecodeSentence(sentencePieceProcessor, ids.Length, ids, out int len, textbuffer);
+			 
+			return Encoding.UTF8.GetString(textbuffer, 0, len);
+		}
+
+		public byte[] Decode2(int[] ids)
+		{
+            var textbuffer = new byte[ids.Length * 50];
+            DecodeSentence(sentencePieceProcessor, ids.Length, ids, out _, textbuffer); 
+			return textbuffer;
+		}
+
+		protected virtual void Dispose(bool bDisposing)
+        {
+			if (this.sentencePieceProcessor != IntPtr.Zero)
+			{ 
+				Dispose(this.sentencePieceProcessor);
+				this.sentencePieceProcessor = IntPtr.Zero;
+			}
+
+			if (bDisposing)
+			{
+				// No need to call the finalizer since we've now cleaned
+				// up the unmanaged memory
+				GC.SuppressFinalize(this);
+			}
+		}
+
+		// This finalizer is called when Garbage collection occurs, but only if
+		// the IDisposable.Dispose method wasn't already called.
+		~SentencePieceDotNET()
+		{
+			Dispose(false);
+		} 
+	}
+}
---
+++ SentencePieceDotNET/test.fsx
@@ -0,0 +1,30 @@
+﻿
+#r @"C:\Users\cybernetic\source\repos\SentencePieceDotNET\bin\x64\Release\netcoreapp3.1\SentencePieceDotNET.dll"
+#r @"C:\Users\cybernetic\source\repos\Prelude\Prelude\bin\Release\net47\prelude.dll"
+
+open Prelude.Common
+
+let sp = new SentencePieceDotNET.SentencePieceDotNET()
+
+
+sp.Load(@"D:\Downloads\NeuralNets\t5-base\spiece.model")
+
+let res = sp.Encode("ö ")
+
+"this is a test of who are you.".Length
+
+let b2 = sp.Decode2 res
+
+sp.Decode res 
+
+let b1 = "ö" |> Strings.toUTF8Bytes
+
+Array.zip b1 b2.[..b1.Length - 1]
+b2.Length
+
+
+let res = sp.Encode "Вагиф Сәмәдоғлу"
+sp.Encode "μ α"
+sp.Encode2 "kö"
+
+"μ α .1 + τ α  Who is Kurt Gödel"
